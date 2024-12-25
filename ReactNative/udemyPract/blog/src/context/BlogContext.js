9dfa18d12fabb9de0e4adba08e41f3ebfb06f425
Natasha <natasha.marie33@gmail.com>
2024-12-25 17:59:02 +0900

    ReactNativeの続き

--- ReactNative/udemyPract/blog/src/context/BlogContext.js
+++ ReactNative/udemyPract/blog/src/context/BlogContext.js
@@ -12,8 +12,8 @@ const blogReducer = (state, action) => {
             });
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload)
-        case 'add_blogpost':
-            return [...state, {id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content}]
+        // case 'add_blogpost':
+        //     return [...state, {id: Math.floor(Math.random() * 99999), title: action.payload.title, content: action.payload.content}]
         default:
             return state;
     }
@@ -34,6 +34,10 @@ const addBlogPost = (dispatch) => {
     return async (title, content, callback) => {
         await jsonServer.post('/blogposts', {title, content})
 
+        const response = await jsonServer.get('/blogposts');
+
+        dispatch({type: 'get_blogposts', payload: response.data})
+
         //if not using api
         // dispatch({type: 'add_blogpost', payload: {title, content}});
         if (callback) {
@@ -43,13 +47,16 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = dispatch => {
-    return (id) => {
+    return async id => {
+        await jsonServer.delete(`/blogposts/${id}`)
         dispatch({type: 'delete_blogpost', payload: id})
     };
 }
 
 const editBlogPost = dispatch => {
-    return(id, title, content, callback) => {
+    return async(id, title, content, callback) => {
+        await jsonServer.put(`/blogposts/${id}`, {title, content})
+
         dispatch({type: 'edit_blogpost', payload: {id, title, content}});
         if (callback) {
             callback();
