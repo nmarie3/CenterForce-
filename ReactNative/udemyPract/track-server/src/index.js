599bfb2114e3be79cf16a9ba25eb0ec9ff8a1740
Natasha <natasha.marie33@gmail.com>
2024-12-27 17:57:45 +0900

    postman+ユーザー承認

--- ReactNative/udemyPract/track-server/src/index.js
+++ ReactNative/udemyPract/track-server/src/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-const mongoUri = 'mongodb+srv://washedonshore:<db_password>@cluster0.z348e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://washedonshore:karipw@cluster0.z348e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi');
